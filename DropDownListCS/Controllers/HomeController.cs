90ac2140179496b57854d2d94080f2b01d9bcf3d
David Ali Zambrano Burgos <davazamb10@outlook.com>
2017-09-28 11:06:38 -0300

    Cambios Modelo ID

--- DropDownListCS/Controllers/HomeController.cs
+++ DropDownListCS/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DropDownListCS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,26 @@ namespace DropDownListCS.Controllers
 {
     public class HomeController : Controller
     {
+        DropDownCSCEntities db = new DropDownCSCEntities();
         public ActionResult Index()
         {
-            return View();
+            List<countries> CountryList = db.countries.ToList();
+            ViewBag.CountryList = new SelectList(CountryList, "name", "name");
+            return View();  
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+        //    return View();
+        //}
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
+        //    return View();
+        //}
     }
 }
---
+++ DropDownListCS/Models/CountryStateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DropDownListCS.Models
+{
+    public class CountryStateViewModel
+    {
+        public int StateId { get; set; }
+        public int CountryID { get; set; }
+    }
+}
